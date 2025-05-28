20abaaed57c35d81a40004a06cd4f7eba903a5e4
davemollen <davemollen@gmail.com>
2025-05-28 08:38:11 +0200

    Add mod gui

--- lv2/dm-TimeWarp.lv2/modgui/javascript.js
+++ lv2/dm-TimeWarp.lv2/modgui/javascript.js
@@ -1,31 +1,51 @@
-function (event, funcs)
-{
-    if (event.type == 'start')
-    {
-        switch (event.symbol) {
-            case 'time':
-                console.log('time', event.value)
-                return
-        }
-        for (var i in event.parameters)
-        {
-            if (event.parameters[i].uri === 'https://github.com/davemollen/dm-TimeWarp#sample')
-            {
-                console.log('sample', event.parameters[i].value);
-                break;
+function(event) {
+  function handle_event(symbol, value) {
+    
+    switch (symbol) {
+        case "record":
+            const record = event.icon.find("[mod-port-symbol=record]");
+            if(value == 1) {
+              record.addClass("on");
+            } else {
+              record.removeClass("on");
             }
-        }
+            break;
+        case "play":
+            const play = event.icon.find("[mod-port-symbol=play]");
+            if(value == 1) {
+              play.addClass("on");
+            } else {
+              play.removeClass("on");
+            }
+            break;
+        case "flush":
+            const flush = event.icon.find("[mod-port-symbol=flush]");
+            if(value == 1) {
+              flush.addClass("on");
+            } else {
+              flush.removeClass("on");
+            }
+            break;
+        case "midi_enabled":
+          const midi = event.icon.find("[mod-port-symbol=midi_enabled]");
+          if(value == 1) {
+            midi.addClass("on");
+          } else {
+            midi.removeClass("on");
+          }
+          break;
+        default:
+            break;
     }
-    else if (event.type == 'change')
-    {
-        switch (event.symbol) {
-            case 'time':
-                console.log('time', event.value)
-                return
-        }
-        if (event.uri === 'https://github.com/davemollen/dm-TimeWarp#sample')
-        {
-            console.log('sample', event.value);
-        }
+  }
+
+  if (event.type == 'start') {
+    const ports = event.ports;
+    for (const p in ports) {
+      handle_event(ports[p].symbol, ports[p].value);
     }
+  }
+  else if (event.type == 'change') {  
+    handle_event(event.symbol, event.value);
+  }
 }
