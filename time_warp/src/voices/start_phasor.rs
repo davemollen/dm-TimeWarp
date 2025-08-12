f63b835d601eb9e3c4023fcfd049ef98a3045bfb
Dave Mollen <davemollen@gmail.com>
2025-08-12 10:20:01 +0200

    Enable stretch when size is below 1

--- time_warp/src/voices/start_phasor.rs
+++ time_warp/src/voices/start_phasor.rs
@@ -15,7 +15,7 @@ impl StartPhasor {
   }
 
   pub fn process(&mut self, speed: f32, time: f32, size: f32, density: f32, stretch: f32) -> f32 {
-    let freq = if size > 0. || density > 0. {
+    let freq = if size < 1. || density > 0. {
       1000. / time * (stretch - 1.)
     } else {
       if speed != self.prev_speed && speed == 1. {
