4b841589a834b722f2f0b633ca2c3f863f52b049
Dave Mollen <davemollen@gmail.com>
2025-08-25 10:12:45 +0200

    Update stopwatch.rs

--- time_warp/src/params/stopwatch.rs
+++ time_warp/src/params/stopwatch.rs
@@ -1,29 +1,34 @@
 pub struct Stopwatch {
-  sample_count: usize,
+  sample_count: Option<usize>,
   sample_rate: f32,
 }
 
 impl Stopwatch {
   pub fn new(sample_rate: f32) -> Self {
     Self {
-      sample_count: 0,
+      sample_count: None,
       sample_rate,
     }
   }
 
   pub fn process(&mut self, start: bool, buffer_size: usize) -> Option<f32> {
     if start {
-      self.sample_count += buffer_size;
+      self.sample_count = match self.sample_count {
+        Some(count) => Some(count + buffer_size),
+        None => Some(0),
+      };
       None
-    } else if self.sample_count == 0 {
+    } else if self.sample_count.map_or(false, |count| count == 0) {
       None
     } else {
-      Some(self.sample_count as f32 / self.sample_rate * 1000.)
+      self
+        .sample_count
+        .map(|count| count as f32 / self.sample_rate * 1000.)
     }
   }
 
   pub fn reset(&mut self) {
-    self.sample_count = 0;
+    self.sample_count = None;
   }
 }
 
@@ -37,6 +42,6 @@ mod tests {
     assert_eq!(stopwatch.process(true, 1), None);
     assert_eq!(stopwatch.process(true, 1), None);
     assert_eq!(stopwatch.process(true, 1), None);
-    assert_eq!(stopwatch.process(false, 1), Some(3000.));
+    assert_eq!(stopwatch.process(false, 1), Some(2000.));
   }
 }
